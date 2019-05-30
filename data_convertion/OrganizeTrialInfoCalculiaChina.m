38930a4ab46f8d8a3172ebad6433deec6c3ad5b3
pinheirochagas <ppinheirochagas@gmail.com>
2019-05-30 15:01:21 -0700

    calculia_china org

--- data_convertion/OrganizeTrialInfoCalculiaChina.m
+++ data_convertion/OrganizeTrialInfoCalculiaChina.m
@@ -14,7 +14,41 @@ for i = 1:length(block_names)
     ntrials = K.nTrials;
     
     trialinfo = K.slist;
-       
+    if ~isfield(trialinfo,  'operator')
+        trialinfo.operator = ones(size(trialinfo,1),1);
+    else
+    end
+    
+    keys = trialinfo.keys;
+    trialinfo.keys = trialinfo.correct;
+    for ii = 1:length(keys)
+        if ~strcmp(keys{ii}, 'noanswer')
+            if strcmp(keys{ii}, 'n')
+                keys{ii} = '1';
+            elseif  strcmp(keys{ii}, 'm')
+                keys{ii} = '2';
+            end
+        else
+        end
+        
+        key = str2num(keys{ii}(1));
+        
+        if ~isempty(key)
+            trialinfo.keys(ii) = key;
+        else
+            trialinfo.keys(ii) = NaN;
+        end
+    end
+    
+    % Accurracy
+    for i = 1:length(trialinfo.keys)
+        if (trialinfo.keys(i) == 1 && trialinfo.correct(i) == 1) || (trialinfo.keys(1) == 2 && trialinfo.correct(i) == 0)
+            trialinfo.Accuracy(i) = 1;
+        else
+            trialinfo.Accuracy(i) = 0;
+        end
+    end
+    
     allonset = nan(ntrials,5);
     for i = 1:ntrials
         for ii = 1:5
