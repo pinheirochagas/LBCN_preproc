6423ca8adace594178f2a0bb4eceb6926b380427
“pinheirochagas” <ppinheirochagas@gmail.com>
2019-06-22 16:53:39 -0700

    modulation

--- data_processing/ElectModulation.m
+++ data_processing/ElectModulation.m
@@ -1,4 +1,4 @@
-function el_modulation = ElectModulation(sbj_name,project_name, block_names, dirs, tag, column, conds, preds, datatype, freq_band, mod_par)
+function reg_stats = ElectModulation(sbj_name,project_name, block_names, dirs, datatype, freq_band, mod_par)
 
 % Get stats parameters
 if isempty(mod_par)
@@ -9,13 +9,13 @@ end
 cfg = [];
 cfg.decimate = false;
 concat_params = genConcatParams(project_name, cfg);
-data_sbj = ConcatenateAll(sbj_name,project_name,block_names,dirs,[],datatype,freq_band,tag, concat_params);
+data_sbj = ConcatenateAll(sbj_name,project_name,block_names,dirs,[],datatype,freq_band,mod_par.tag, concat_params);
 
 % Run regression across fields - 1 predictor!
 conds = mod_par.conds;
 reg_stats = [];
 for ii = 1%:length(mod_par.preds.(conds{i}))
-    for imod = 1:length(mod_par.dep_vars.(mod_par.conds{ii}))
+    for imod = 1:length(mod_par.dep_vars.(conds{ii}))
         for el = 1:length(data_sbj.trialinfo_all)
             dep_n = mod_par.dep_vars.(conds{ii}){imod};
             pred_n = mod_par.preds.(conds{ii}){imod};
