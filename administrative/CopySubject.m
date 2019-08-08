5243d36aa809b554063906fc027c146586edf1c8
pinheirochagas <ppinheirochagas@gmail.com>
2019-08-08 15:33:14 -0700

    connectivity

--- administrative/CopySubject.m
+++ administrative/CopySubject.m
@@ -4,7 +4,7 @@ function CopySubject(sbj_name, s_psychData, d_psychData, s_neuralData, d_neuralD
 
 %% neuralData_folders
 if strcmp(neuralData_folders, 'all')
-    neuralData_folders = {'originalData', 'CARData', 'SpecData', 'HFBData'};
+    neuralData_folders = {'originalData', 'CARData', 'SpecData', 'BandData'};
 else
 end
 
@@ -14,11 +14,11 @@ for i = 1:length(block_names)
     tmp_psych_folder_d = [d_psychData filesep sbj_name filesep bn];
     tmp_psych_folder_s = [s_psychData filesep sbj_name filesep bn];
     
-    if ~exist(tmp_psych_folder_d)
-        mkdir(tmp_psych_folder_d)
-        copyfile(tmp_psych_folder_s, tmp_psych_folder_d)
-    else
-    end
+    mkdir(tmp_psych_folder_d)
+    copyfile(tmp_psych_folder_s, tmp_psych_folder_d)
+%     if ~exist(tmp_psych_folder_d)
+%     else
+%     end
     
     %% Copy neural data
     for ii = 1:length(neuralData_folders)
@@ -26,25 +26,28 @@ for i = 1:length(block_names)
         if strcmp(neuralData_folders{ii}, 'CARData')
             tmp_neural_folder_d = [d_neuralData filesep neuralData_folders{ii} filesep 'CAR' filesep sbj_name filesep bn];
             tmp_neural_folder_s = [s_neuralData filesep neuralData_folders{ii} filesep 'CAR' filesep sbj_name filesep bn];
+        elseif strcmp(neuralData_folders{ii}, 'BandData')
+            tmp_neural_folder_d = [d_neuralData filesep neuralData_folders{ii} filesep 'HFB' filesep sbj_name filesep bn];
+            tmp_neural_folder_s = [s_neuralData filesep neuralData_folders{ii} filesep 'HFB' filesep sbj_name filesep bn];
         else
             tmp_neural_folder_d = [d_neuralData filesep neuralData_folders{ii} filesep sbj_name filesep bn];
-            tmp_neural_folder_s = [s_neuralData filesep neuralData_folders{ii} filesep sbj_name filesep bn];
+            tmp_neural_folder_s = [s_neuralData filesep neuralData_folders{ii} filesep sbj_name filesep bn];            
         end
-        if ~exist(tmp_neural_folder_d)
-            disp(['copying ' neuralData_folders{ii} ' ' sbj_name])
-            mkdir(tmp_neural_folder_d)
-            copyfile(tmp_neural_folder_s, tmp_neural_folder_d)
-        else
-        end
+        disp(['copying ' neuralData_folders{ii} ' ' sbj_name])
+        mkdir(tmp_neural_folder_d)
+        copyfile(tmp_neural_folder_s, tmp_neural_folder_d)
+        %         if ~exist(tmp_neural_folder_d)
+        %         else
+        %         end
     end
     
     %% Copy globalVar
     global_var_s_path = sprintf('%s/originalData/%s/global_%s_%s_%s.mat',s_neuralData,sbj_name,project_name,sbj_name,bn);
     global_var_d_path = sprintf('%s/originalData/%s/global_%s_%s_%s.mat',d_neuralData,sbj_name,project_name,sbj_name,bn);
     copyfile(global_var_s_path, global_var_d_path)
-    if ~exist(global_var_d_path)
-    else
-    end
+%     if ~exist(global_var_d_path)
+%     else
+%     end
     
 end
 end
