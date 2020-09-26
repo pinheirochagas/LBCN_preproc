d898e985e7dba89bc1f777b979c614d4af4018e5
Chao Zhang <babaoriley1@163.com>
2020-09-26 12:04:14 -0700

    Update getmgrid.m

--- administrative/getmgrid.m
+++ administrative/getmgrid.m
@@ -6,6 +6,6 @@ subj = subj(end).name;
 subDir = [subDir subj];
 
 [mgrid_coord, elect_names, elecRgb,~,elecPresent] = mgrid2matlab_custom(subj,0, dirs.freesurfer);
-elecPresent = elecPresent_exceptions(subj,elecPresent);
-mgrid_coord = mgrid_coord(elecPresent==1,:);
+% elecPresent = elecPresent_exceptions(subj,elecPresent);
+% mgrid_coord = mgrid_coord(elecPresent==1,:);
 end
