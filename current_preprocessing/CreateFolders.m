8ec9c77486d881fd741b27903dfb4d340b2441d9
pinheirochagas <ppinheirochagas@gmail.com>
2020-11-05 14:13:46 -0300

    rest creat folders

--- current_preprocessing/CreateFolders.m
+++ current_preprocessing/CreateFolders.m
@@ -138,7 +138,7 @@ for bn = 1:length(block_name)
                     globalVar.iEEG_data_server_path = [PATHNAME, FILENAME];
                 else
                 end
-                if ~strcmp(project_name, 'Rest') || ~strcmp(project_name, 'Rest_pupil')
+                if ~contains(project_name, 'Rest')
                     % Behavioral data
                     waitfor(msgbox(['Choose file of the behavioral data on the server for block ' block_name{bn}]));
                     [FILENAME, PATHNAME] = uigetfile(['/Volumes/neurology_jparvizi$/' sbj_folder_name]);
